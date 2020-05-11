9bab8c6a66c3d7cd03450def05661d4b3f69ad0e
hien <hien07072001@gmail.com>
2020-05-11 14:47:09 +0700

    add

---
+++ src/main/java/com/codegym/model/User.java
@@ -0,0 +1,30 @@
+package com.codegym.model;
+
+public class User {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
